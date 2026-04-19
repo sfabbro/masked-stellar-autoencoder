ddc41ffbfb7590cd7f619f4bea3a97243c74a3b7
sfabbro <1484206+sfabbro@users.noreply.github.com>
2026-04-19 16:35:07 +0000

    perf: move HDF5 context manager outside of loop
    Moved the `with h5py.File('gaia/source_ids_x_file_names.h5', 'a') as hf:`
    context manager outside the loop over `source_files` in
    `data/source_ids_x_file_names.py` to prevent repeatedly opening and
    closing the target HDF5 file.
    
    Also removed the redundant `f.close()` inside the loop, since `f` is
    managed by a context manager. Added the generated dummy test path `gaia/`
    to `.gitignore`.

--- data/source_ids_x_file_names.py
+++ data/source_ids_x_file_names.py
@@ -4,13 +4,12 @@ import glob
 import pandas as pd
 
 source_files = np.sort(glob.glob('gaia/GaiaSource/*'))
-for file in source_files:
-    with h5py.File(file,'r') as f:
-        ids = f['source_id'][:]
-        xpq = f['has_xp_continuous'][:]
-        filename = file.split('/')[-1].split('.')[0]
-        data_dict = {'source_id': ids, 'has_xp_coeffs': xpq}
-        dataset_to_save = pd.DataFrame(data_dict)
-        with h5py.File('gaia/source_ids_x_file_names.h5', 'a') as hf:
+with h5py.File('gaia/source_ids_x_file_names.h5', 'a') as hf:
+    for file in source_files:
+        with h5py.File(file, 'r') as f:
+            ids = f['source_id'][:]
+            xpq = f['has_xp_continuous'][:]
+            filename = file.split('/')[-1].split('.')[0]
+            data_dict = {'source_id': ids, 'has_xp_coeffs': xpq}
+            dataset_to_save = pd.DataFrame(data_dict)
             hf.create_dataset(filename, data=dataset_to_save)
-        f.close()
